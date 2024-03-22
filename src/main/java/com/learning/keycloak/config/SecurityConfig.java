fdc946cad76124b808b33478329dbb22012fbb14
scepter <chukwunenyeebuka@gmail.com>
2024-03-22 21:46:27 +0100

    First commit

---
+++ src/main/java/com/learning/keycloak/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.learning.keycloak.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     http.csrf(AbstractHttpConfigurer::disable)
+             .sessionManagement(c -> c.sessionCreationPolicy(STATELESS))
+             .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+             .oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()));
+     return http.build();
+    }
+}
